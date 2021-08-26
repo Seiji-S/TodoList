a55ea1b0d0d98437a522fc0ea1ef64cfade846a8
seiji-s <seiji.swingl@gmail.com>
2021-08-26 16:43:50 -0700

    added task mapping and creation

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,28 @@
 pragma solidity ^0.5.0;
+
 //declare version of solidity we want to use
 
 contract TodoList {
-  uint public taskCount = 0;
-  //state variable that belongs on the blockchain
+    uint256 public taskCount = 0;
+    //state variable that belongs on the blockchain
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out my neat PC");
+        //create initial task
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        //add to task count every time function runs
+        tasks[taskCount] = Task(taskCount, _content, false);
+        //populate tasks by creating new tasks with attributes defined in struct
+    }
 }
